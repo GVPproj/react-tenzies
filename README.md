9631cc60e8a040ae97d35da1ebab69f8b115bc3f
Graham Van Pelt <grahamvanpelt@gmail.com>
2022-11-14 08:59:34 -0800

    responsive layout added, custom font and color scheme also

--- README.md
+++ README.md
@@ -6,6 +6,5 @@ https://www.figma.com/file/vQmB9c6zSA9qK4mNlBEWqc/Tenzies-(Copy)?node-id=0%3A1
 
 ## ToDo
 
-- track time it took to win
-- save best time to local storage
-- more responsive layout clear
+- info pane
+- save best time and roll count to local storage
--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,28 @@ function App() {
   const [dice, setDice] = useState(allNewDice())
   // set win state (false by default)
   const [tenzies, setTenzies] = useState(false)
-  const [count, setCount] = useState(1)
+  // count rolls
+  const [count, setCount] = useState(0)
+  // timer time (number)
+  const [time, setTime] = useState(0)
+  // timer running?  (boolean)
+  const [running, setRunning] = useState(false)
 
+  // timer side effect
+  useEffect(() => {
+    let interval
+    // runs every 10 ms
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10)
+      }, 10)
+    } else if (!running) {
+      clearInterval(interval) // stops interval from running based on state
+    }
+    return () => clearInterval(interval)
+  }, [running]) // useEffect when {running} is true
+
+  // check state of game
   useEffect(() => {
     const stillPlaying = dice.find((die) => die.isHeld === false)
 
@@ -18,8 +38,8 @@ function App() {
     const allSameValue = dice.every((die) => die.value === firstValue)
 
     if (!stillPlaying && allSameValue) {
+      setRunning(false)
       setTenzies(true)
-      console.log("winner")
     }
   }, [dice])
 
@@ -43,7 +63,8 @@ function App() {
 
   // set our state
   function rollDice() {
-    setCount(prevCount => prevCount + 1)
+    setRunning(true)
+    setCount((prevCount) => prevCount + 1)
     console.log(count)
     setDice((oldDice) =>
       oldDice.map((die) => {
@@ -56,9 +77,12 @@ function App() {
     setCount(1)
     setTenzies(!tenzies)
     setDice(allNewDice())
+    setTime(0)
+    setRunning(false)
   }
 
   function holdDice(id) {
+    !tenzies ? setRunning(true) : setRunning(false)
     setDice((oldDice) =>
       oldDice.map((die) => {
         return die.id === id ? { ...die, isHeld: !die.isHeld } : die
@@ -79,16 +103,46 @@ function App() {
   return (
     <main>
       {tenzies && <Confetti />}
+
       <h1 className="title">Tenzies</h1>
       {!tenzies ? (
         <p className="instructions">
-          Roll until all dice are the same. Click each die to freeze it at its
-          current value between rolls.
+          Roll until all dice match. Click dice to freeze between rolls.
         </p>
       ) : (
-        <h2>You won with {count} rolls of the dice!</h2>
+        <p>WINNER! Nice one, you.</p>
       )}
       <div className="diceContainer">{dieElements}</div>
+      {!tenzies ? (
+        <div className="stopwatch">
+          {running ? (
+            <div className="numbers">
+              <div>
+                <span>Rolls: </span>
+                <span>{count}</span>
+              </div>
+              <div>
+                <span>Current time: </span>
+                <span>
+                  {("0" + Math.floor((time / 1000) % 60)).slice(-2)} seconds
+                </span>
+              </div>
+            </div>
+          ) : (
+            <span>Click dice / roll to begin!</span>
+          )}
+        </div>
+      ) : (
+        <p>
+          You won in{" "}
+          <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>{" "}
+          seconds with {count} rolls of the dice!
+          <br />
+          Best time:{" "}
+          <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span> seconds / Fewest
+          rolls: {count}
+        </p>
+      )}
       <button className="roll shadow" onClick={tenzies ? newGame : rollDice}>
         {tenzies ? "New Game?" : "Roll"}
       </button>
