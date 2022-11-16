350653ca6a228d14880eeb887e3d0b8cb3cea140
GVPproj <69814261+GVPproj@users.noreply.github.com>
2022-11-15 16:26:53 -0800

    public folder die images

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,7 @@
 export default function Die(props){
     const styles = {
         // backgroundColor: props.isHeld ? "#41ead4" : "white"
-        backgroundImage: props.isHeld ? `url(src/dice_png/inverted-dice-${props.value}.png)` : `url(src/dice_png/${props.value}.png)`,
+        backgroundImage: props.isHeld ? `url(/dice_png/inverted-dice-${props.value}.png)` : `url(/dice_png/${props.value}.png)`,
         backgroundSize: "100%"
     }
 
